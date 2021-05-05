ce7d91549e5dbc57c98f741c9bedd35625373a60
Eda <eda.isaku@yahoo.com>
2021-05-05 16:13:45 +0200

    Ädded Header and Button Components

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+export const Header = ({ title }) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+      <Button />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task trucker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+//CSS in JS
+// const headingStyle = {
+//   color: "green",
+//   backgroundColor: "black",
+// };
+
+export default Header;
