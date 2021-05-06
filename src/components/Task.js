3d20aaa2feeeff924297ea8eb8258207e609ddb0
Eda <eda.isaku@yahoo.com>
2021-05-06 09:48:59 +0200

    Added icon

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,17 @@
+import { FaTimes } from "react-icons/fa";
 const Task = ({ task }) => {
   return (
     <div className="task">
-      <h3>{task.text}</h3>
+      <h3>
+        {task.text}
+        <FaTimes style={iconStyle} />{" "}
+      </h3>
       <p>{task.day}</p>
     </div>
   );
+};
+const iconStyle = {
+  color: "red",
 };
 
 export default Task;
